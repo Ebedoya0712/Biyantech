afb5fab6a93679338130fc18b73c18c5dec18b57
@CML-ebedoya <ebedoya@cmlexports.com>
2025-05-14 10:08:51 -0400

    Formulario del registro de un curso III

--- admin_metronic/src/app/modules/course/course-add/course-add.component.ts
+++ admin_metronic/src/app/modules/course/course-add/course-add.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CourseService } from '../service/course.service';
+import { Toaster } from 'ngx-toast-notifications';
 
 @Component({
   selector: 'app-course-add',
@@ -14,8 +15,16 @@ export class CourseAddComponent implements OnInit {
   instructores:any = [];
 
   isLoading:any;
+  FILE_PORTADA:any = null;
+  IMAGEN_PREVISUALIZA:any = null;
+
+  text_requiriments:any = null;
+  requiriments:any = [];
+  text_what_is_for:any = null;
+  what_is_fors: any = [];
   constructor(
     public courseService: CourseService,
+    public toaster: Toaster,
   ) { }
 
   ngOnInit(): void {
@@ -33,11 +42,46 @@ export class CourseAddComponent implements OnInit {
           console.log(VALUE);
           this.subcategories_back = this.subcategories.filter((item:any) => item.categorie_id == VALUE);
       }
+
+      addRequiriments(){
+        if(!this.text_requiriments){
+            this.toaster.open({text: 'NECESITAS INGRESAR UN REQUERIMIENTO',caption: 'VALIDACION', type: 'danger'});
+            return;
+        }
+          this.requiriments.push(this.text_requiriments);
+          this.text_requiriments = null;
+      }
+      addWhatIsFor(){
+        if(!this.text_what_is_for){
+          this.toaster.open({text: 'NECESITAS INGRESAR UNA PERSONA DIRIGIDA',caption: 'VALIDACION', type: 'danger'});
+            return;
+        }
+          this.what_is_fors.push(this.text_what_is_for);
+          this.text_what_is_for = null;
+      }
+
+      removeRequiriment(index:number){
+        this.requiriments.splice(index,1);
+      }
+      removeWhatIsFor(index:number){
+        this.what_is_fors.splice(index,1);
+      }
   save(){
           
   }
 
   processFile($event:any){
-
+      if($event.target.files[0].type.indexOf("image") < 0){
+      this.toaster.open({text: 'SOLAMENTE SE ACEPTAN IMAGENES', caption:'MENSAJE DE VALIDACIÓN',type: 'danger'})
+      return;
+    }
+    this.FILE_PORTADA = $event.target.files[0];
+    let reader = new FileReader();
+    reader.readAsDataURL(this.FILE_PORTADA);
+    reader.onloadend = () => this.IMAGEN_PREVISUALIZA = reader.result;
+    this.courseService.isLoadingSubject.next(true);
+    setTimeout(() => {
+      this.courseService.isLoadingSubject.next(false);
+    }, 50);
   }
 }
