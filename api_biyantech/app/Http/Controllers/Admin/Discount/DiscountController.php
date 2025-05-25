a9ee34cfebb6d502ed7f7e3848ee89428d051dd7
@CML-ebedoya <ebedoya@cmlexports.com>
2025-05-25 13:54:25 -0400

    Registro de una campaña de descuento

--- api_biyantech/app/Http/Controllers/Admin/Discount/DiscountController.php
+++ api_biyantech/app/Http/Controllers/Admin/Discount/DiscountController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers\Admin\Discount;
 
-use App\Http\Controllers\Controller;
-use App\Http\Resources\Discount\DiscountCollection;
-use App\Models\Discount\Discount;
-use App\Models\Discount\DiscountCategorie;
-use App\Models\Discount\DiscountCourse;
 use Illuminate\Http\Request;
+use App\Models\Discount\Discount;
+use App\Http\Controllers\Controller;
+use App\Models\Discount\DiscountCourse;
+use App\Models\Discount\DiscountCategorie;
+use App\Http\Resources\Discount\DiscountResource;
+use App\Http\Resources\Discount\DiscountCollection;
 
 class DiscountController extends Controller
 {
@@ -20,7 +21,7 @@ class DiscountController extends Controller
     {
         $search = $request->search;
         $state = $request->state;
-        //filterAdvance($search,$state)->
+        // filterAdvance($search,$state)->
         $discounts = Discount::orderBy("id","desc")->get();
 
         return response()->json(["message" => 200, "discounts" => DiscountCollection::make($discounts)]);
@@ -44,60 +45,61 @@ class DiscountController extends Controller
      */
     public function store(Request $request)
     {
-        //discount_type 1 es curso y 2 es categoria
+        //discount_type 1 es course y 2 es categoria
 
-        if($request->discount_type == 1){//VALIDACION A NIVEL DE CURSO
-            foreach($request->course_selected as $key => $course){
-                $IS_DISCOUNT_START_DATE = Discount::where("discount_type",$request->$request->discount_type)->whereHas("courses",function($q) use($course){
+        if($request->discount_type == 1){//VALIDACIÓN A NIVEL DE CURSO
+            foreach ($request->course_selected as $key => $course) {
+                $IS_DISCOUNT_START_DATE = Discount::where("type_campaing",$request->type_campaing)->where("discount_type",$request->discount_type)->whereHas("courses",function($q) use($course){
                     return $q->where("course_id",$course["id"]);
                 })->whereBetween("start_date",[$request->start_date,$request->end_date])->first();
 
-                $IS_DISCOUNT_END_DATE = Discount::where("discount_type",$request->$request->discount_type)->whereHas("courses",function($q) use($course){
+                $IS_DISCOUNT_END_DATE = Discount::where("type_campaing",$request->type_campaing)->where("discount_type",$request->discount_type)->whereHas("courses",function($q) use($course){
                     return $q->where("course_id",$course["id"]);
                 })->whereBetween("end_date",[$request->start_date,$request->end_date])->first();
+
                 if($IS_DISCOUNT_START_DATE || $IS_DISCOUNT_END_DATE){
-                    return response()->json(["message" => 403, "message_text" => "EL CURSO ".$course["title"]."YA SE ENCUENTRA EN UNA CAMPAÑA DE DESCUENTO - ".($IS_DISCOUNT_START_DATE ? $IS_DISCOUNT_START_DATE->id : '-').($IS_DISCOUNT_END_DATE ? $IS_DISCOUNT_END_DATE->id : '')]);
+                    return response()->json(["message" => 403, "message_text" => "EL CURSO '".$course["title"]."' YA SE ENCUENTRA EN UNA CAMPAÑA DE DESCUENTO - ".($IS_DISCOUNT_START_DATE ? $IS_DISCOUNT_START_DATE->id : '-').($IS_DISCOUNT_END_DATE ? $IS_DISCOUNT_END_DATE->id : '')]);
                 }
             }
         }
 
-        if($request->discount_type == 2){//VALIDACION A NIVEL DE CATEGORIA
-            foreach($request->categorie_selected as $key => $categorie){
-                $IS_DISCOUNT_START_DATE = Discount::where("discount_type",$request->$request->discount_type)->whereHas("categories",function($q) use($categorie){
+        if($request->discount_type == 2){//VALIDACIÓN A NIVEL DE CATEGORIA  
+            foreach ($request->categorie_selected as $key => $categorie) {
+                $IS_DISCOUNT_START_DATE = Discount::where("type_campaing",$request->type_campaing)->where("discount_type",$request->discount_type)->whereHas("categories",function($q) use($categorie){
                     return $q->where("categorie_id",$categorie["id"]);
                 })->whereBetween("start_date",[$request->start_date,$request->end_date])->first();
 
-                $IS_DISCOUNT_END_DATE = Discount::where("discount_type",$request->$request->discount_type)->whereHas("categories",function($q) use($categorie){
+                $IS_DISCOUNT_END_DATE = Discount::where("type_campaing",$request->type_campaing)->where("discount_type",$request->discount_type)->whereHas("categories",function($q) use($categorie){
                     return $q->where("categorie_id",$categorie["id"]);
                 })->whereBetween("end_date",[$request->start_date,$request->end_date])->first();
+
                 if($IS_DISCOUNT_START_DATE || $IS_DISCOUNT_END_DATE){
-                    return response()->json(["message" => 403, "message_text" => "LA CATEGORIA".$categorie["name"]."YA SE ENCUENTRA EN UNA CAMPAÑA DE DESCUENTO - ".($IS_DISCOUNT_START_DATE ? $IS_DISCOUNT_START_DATE->id : '-').($IS_DISCOUNT_END_DATE ? $IS_DISCOUNT_END_DATE->id : '')]);
+                    return response()->json(["message" => 403, "message_text" => "LA CATEGORIA '".$categorie["name"]."' YA SE ENCUENTRA EN UNA CAMPAÑA DE DESCUENTO - ".($IS_DISCOUNT_START_DATE ? $IS_DISCOUNT_START_DATE->id : '-').($IS_DISCOUNT_END_DATE ? $IS_DISCOUNT_END_DATE->id : '')]);
                 }
             }
         }
-        
 
         $request->request->add(["code" => uniqid()]);
         $discount = Discount::create($request->all());
 
-        if($request->discount_type == 1){
-            foreach($request->course_selected as $key => $course){
-                DiscountCourse::create([
-                    "discount_id" => $discount->id,
-                    "course_id" => $course["id"],
-                ]);
+        if($request->discount_type == 1){// 1 es course
+            foreach ($request->course_selected as $key => $course) {
+               DiscountCourse::create([
+                "discount_id" => $discount->id,
+                "course_id" => $course["id"],
+               ]);
+            }
         }
-    }
-        
-        if($request->discount_type == 2){
-            foreach($request->categorie_selected as $key => $categorie){
+        if($request->discount_type == 2){// 2 es categoria
+            foreach ($request->categorie_selected as $key => $categorie) {
                 DiscountCategorie::create([
                     "discount_id" => $discount->id,
                     "categorie_id" => $categorie["id"],
                 ]);
             }
         }
-            return response()->json(["message" => 200]);
+
+        return response()->json(["message" => 200 ]);
     }
 
     /**
@@ -108,7 +110,9 @@ class DiscountController extends Controller
      */
     public function show($id)
     {
-        //
+        $discount = Discount::findOrFail($id);
+
+        return response()->json(["discount" =>DiscountResource::make($discount)]);
     }
 
     /**
@@ -131,66 +135,68 @@ class DiscountController extends Controller
      */
     public function update(Request $request, $id)
     {
-        if($request->discount_type == 1){//VALIDACION A NIVEL DE CURSO
-            foreach($request->course_selected as $key => $course){
-                $IS_DISCOUNT_START_DATE = Discount::where("id","<>",$id)->where("discount_type",$request->$request->discount_type)->whereHas("courses",function($q) use($course){
+        if($request->discount_type == 1){//VALIDACIÓN A NIVEL DE CURSO
+            foreach ($request->course_selected as $key => $course) {
+                $IS_DISCOUNT_START_DATE = Discount::where("id","<>",$id)->where("type_campaing",$request->type_campaing)->where("discount_type",$request->discount_type)->whereHas("courses",function($q) use($course){
                     return $q->where("course_id",$course["id"]);
                 })->whereBetween("start_date",[$request->start_date,$request->end_date])->first();
 
-                $IS_DISCOUNT_END_DATE = Discount::where("id","<>",$id)->where("discount_type",$request->$request->discount_type)->whereHas("courses",function($q) use($course){
+                $IS_DISCOUNT_END_DATE = Discount::where("id","<>",$id)->where("type_campaing",$request->type_campaing)->where("discount_type",$request->discount_type)->whereHas("courses",function($q) use($course){
                     return $q->where("course_id",$course["id"]);
                 })->whereBetween("end_date",[$request->start_date,$request->end_date])->first();
+
                 if($IS_DISCOUNT_START_DATE || $IS_DISCOUNT_END_DATE){
-                    return response()->json(["message" => 403, "message_text" => "EL CURSO ".$course["title"]."YA SE ENCUENTRA EN UNA CAMPAÑA DE DESCUENTO - ".($IS_DISCOUNT_START_DATE ? $IS_DISCOUNT_START_DATE->id : '-').($IS_DISCOUNT_END_DATE ? $IS_DISCOUNT_END_DATE->id : '')]);
+                    return response()->json(["message" => 403, "message_text" => "EL CURSO '".$course["title"]."' YA SE ENCUENTRA EN UNA CAMPAÑA DE DESCUENTO - ".($IS_DISCOUNT_START_DATE ? $IS_DISCOUNT_START_DATE->id : '-').($IS_DISCOUNT_END_DATE ? $IS_DISCOUNT_END_DATE->id : '')]);
                 }
             }
         }
 
-        if($request->discount_type == 2){//VALIDACION A NIVEL DE CATEGORIA
-            foreach($request->categorie_selected as $key => $categorie){
-                $IS_DISCOUNT_START_DATE = Discount::where("id","<>",$id)->where("discount_type",$request->$request->discount_type)->whereHas("categories",function($q) use($categorie){
+        if($request->discount_type == 2){//VALIDACIÓN A NIVEL DE CATEGORIA  
+            foreach ($request->categorie_selected as $key => $categorie) {
+                $IS_DISCOUNT_START_DATE = Discount::where("id","<>",$id)->where("type_campaing",$request->type_campaing)->where("discount_type",$request->discount_type)->whereHas("categories",function($q) use($categorie){
                     return $q->where("categorie_id",$categorie["id"]);
                 })->whereBetween("start_date",[$request->start_date,$request->end_date])->first();
 
-                $IS_DISCOUNT_END_DATE = Discount::where("id","<>",$id)->where("discount_type",$request->$request->discount_type)->whereHas("categories",function($q) use($categorie){
+                $IS_DISCOUNT_END_DATE = Discount::where("id","<>",$id)->where("type_campaing",$request->type_campaing)->where("discount_type",$request->discount_type)->whereHas("courses",function($q) use($categorie){
                     return $q->where("categorie_id",$categorie["id"]);
                 })->whereBetween("end_date",[$request->start_date,$request->end_date])->first();
+
                 if($IS_DISCOUNT_START_DATE || $IS_DISCOUNT_END_DATE){
-                    return response()->json(["message" => 403, "message_text" => "LA CATEGORIA".$categorie["name"]."YA SE ENCUENTRA EN UNA CAMPAÑA DE DESCUENTO - ".($IS_DISCOUNT_START_DATE ? $IS_DISCOUNT_START_DATE->id : '-').($IS_DISCOUNT_END_DATE ? $IS_DISCOUNT_END_DATE->id : '')]);
+                    return response()->json(["message" => 403, "message_text" => "LA CATEGORIA '".$categorie["name"]."' YA SE ENCUENTRA EN UNA CAMPAÑA DE DESCUENTO - ".($IS_DISCOUNT_START_DATE ? $IS_DISCOUNT_START_DATE->id : '-').($IS_DISCOUNT_END_DATE ? $IS_DISCOUNT_END_DATE->id : '')]);
                 }
             }
         }
-        
 
         $request->request->add(["code" => uniqid()]);
         $discount = Discount::findOrFail($id);
         $discount->update($request->all());
 
-        foreach($discount->courses as $key => $courseD){
+
+        foreach ($discount->courses as $key => $courseD) {
             $courseD->delete();
         }
-        foreach($discount->categories as $key => $categorieD){
+        foreach ($discount->categories as $key => $categorieD) {
             $categorieD->delete();
         }
 
-        if($request->discount_type == 1){
-            foreach($request->course_selected as $key => $course){
-                DiscountCourse::create([
-                    "discount_id" => $discount->id,
-                    "course_id" => $course["id"],
-                ]);
+        if($request->discount_type == 1){// 1 es course
+            foreach ($request->course_selected as $key => $course) {
+               DiscountCourse::create([
+                "discount_id" => $discount->id,
+                "course_id" => $course["id"],
+               ]);
+            }
         }
-    }
-        
-        if($request->discount_type == 2){
-            foreach($request->categorie_selected as $key => $categorie){
+        if($request->discount_type == 2){// 2 es categoria
+            foreach ($request->categorie_selected as $key => $categorie) {
                 DiscountCategorie::create([
                     "discount_id" => $discount->id,
                     "categorie_id" => $categorie["id"],
                 ]);
             }
         }
-            return response()->json(["message" => 200]);
+
+        return response()->json(["message" => 200 ]);
     }
 
     /**
--- api_biyantech/app/Http/Resources/Discount/DiscountResource.php
+++ api_biyantech/app/Http/Resources/Discount/DiscountResource.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Resources\Discount;
 
-use Illuminate\Http\Resources\Json\JsonResource;
 use Carbon\Carbon;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class DiscountResource extends JsonResource
 {
