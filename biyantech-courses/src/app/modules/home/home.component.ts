b4c0146999ef1240566cbe1f6efc0dd777f8f3af
@CML-ebedoya <ebedoya@cmlexports.com>
2025-06-05 09:39:10 -0400

    Campaña de descuento flash

--- biyantech-courses/src/app/modules/home/home.component.ts
+++ biyantech-courses/src/app/modules/home/home.component.ts
@@ -4,6 +4,7 @@ import { HomeService } from './services/home.service';
 declare var $:any ;
 declare function HOMEINIT([]):any;
 declare function banner_home():any;
+declare function countdownT():any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -17,6 +18,10 @@ export class HomeComponent implements OnInit{
   DESCOUNT_BANNER:any = null;
   DESCOUNT_BANNER_COURSES:any = [];
 
+  DESCOUNT_FLASH:any = null;
+  DESCOUNT_FLASH_COURSES:any = [];
+
+
   constructor(
     public homeService: HomeService,
   ){
@@ -25,26 +30,37 @@ export class HomeComponent implements OnInit{
     }, 50);
   }
 
-    ngOnInit(): void {
-        this.homeService.home().subscribe((resp:any) => {
-            console.log(resp);
-            this.CATEGORIES = resp.categories;
-            this.COURSES_HOME = resp.courses_home.data;
-            this.group_courses_categories = resp.group_courses_categories;
-            this.DESCOUNT_BANNER = resp. DESCOUNT_BANNER;
-            this.DESCOUNT_BANNER_COURSES = resp. DESCOUNT_BANNER_COURSES;
+  ngOnInit(): void {
+    
+    this.homeService.home().subscribe((resp:any) => {
+      console.log(resp);
+      this.CATEGORIES = resp.categories;
+      this.COURSES_HOME = resp.courses_home.data;
+      this.group_courses_categories = resp.group_courses_categories;
+      this.DESCOUNT_BANNER = resp.DESCOUNT_BANNER;
+      this.DESCOUNT_BANNER_COURSES = resp.DESCOUNT_BANNER_COURSES;
+      this.DESCOUNT_FLASH = resp.DESCOUNT_FLASH;
+      this.DESCOUNT_FLASH_COURSES = resp.DESCOUNT_FLASH_COURSES;
+      setTimeout(() => {
+        banner_home();
+        countdownT();
+      }, 50);
+    })
+    
+  }
 
-            setTimeout(() => {
-                banner_home();
-            }, 50);
-        });
+  getNewTotal(COURSE:any,DESCOUNT_BANNER:any){
+    if(DESCOUNT_BANNER.type_discount == 1){
+      return COURSE.precio_usd - COURSE.precio_usd*(DESCOUNT_BANNER.discount*0.01);
+    }else{
+      return COURSE.precio_usd - DESCOUNT_BANNER.discount;
     }
+  }
 
-    getNewTotal(COURSE:any, DESCOUNT_BANNER:any) {
-      if(DESCOUNT_BANNER.type_discount === 1) {
-        return COURSE.precio_usd - COURSE.precio_usd * (DESCOUNT_BANNER.discount * 0.01);
-      }else{
-        return COURSE.precio_usd - DESCOUNT_BANNER.discount;
-      }
+  getTotalPriceCourse(COURSE:any){
+    if(COURSE.discount_g){
+      return this.getNewTotal(COURSE,COURSE.discount_g);
+    }
+    return COURSE.precio_usd;
   }
 }
